972823cf70950a35b917e9722c05ec952087b856
YosefTamer <programmeryosef@gmail.com>
2026-02-11 07:20:00 +0200

    Refactor PollStation contract for efficiency
    Updated function parameters to use 'calldata' for better gas efficiency and modified contract declaration.

--- PollStation.sol
+++ PollStation.sol
@@ -1,26 +1,32 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-pragma solidity ^0.8.0;
 
-contract PollStation{
-
+contract PollStation {
     string[] public candidateNames;
     mapping(string => uint256) voteCount;
+    mapping(address => bool) hasVoting;
+    mapping(string => bool) isRegistered;
 
-    function addCandidateNames(string memory _candidateNames) public{
+    function addCandidateNames(string calldata _candidateNames) public{
         candidateNames.push(_candidateNames);
         voteCount[_candidateNames] = 0;
+        isRegistered[_candidateNames] = true;
+        
     }
     
     function getcandidateNames() public view returns (string[] memory){
         return candidateNames;
     }
 
-    function vote(string memory _candidateNames) public{
+    function vote(string calldata _candidateNames) public{
+        require(isRegistered[_candidateNames] == true, "Not a Candidate");
+        require(!hasVoting[msg.sender],"Already Voted");
+        hasVoting[msg.sender] = true;
         voteCount[_candidateNames] += 1;
     }
 
-    function getVote(string memory _candidateNames) public view returns (uint256){
+    function getVote(string calldata _candidateNames) public view returns (uint256){
         return voteCount[_candidateNames];
     }
 
